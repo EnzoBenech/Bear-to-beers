2420d1eac94e365c4607b9a88cb485fc545d0774
ArmandRazafindraibe <155958680+ArmandRazafindraibe@users.noreply.github.com>
2024-06-13 15:39:23 +0200

    Barre de vie

---
+++ Bear to beers/Assets/Ennemies/Barre de vie/Assets/Health.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Health : MonoBehaviour
+{
+    public float health = 100;
+    public float maxHealth =100;
+
+    public Image healthBarImage;
+    public TextMeshProUGUI healthText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBarImage.fillAmount = health / maxHealth;
+        healthText.text = health + " / " + maxHealth;
+        health = Mathf.Clamp(health, 0f, maxHealth);
+    }
+
+    public void DamageButton(int damageAmount)
+    {
+        health -= damageAmount;
+    }
+
+    public void HealButton(int healAmount)
+    {
+        health += healAmount;
+    }
+}
